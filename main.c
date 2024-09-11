60294a16b79e3826616b1267b2f1a22b3f2dc9e7
OttinLuis <ottoluis135@gmail.com>
2024-09-11 05:51:40 -0300

    Add files via upload
    calculadora #C

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(int argc, char *argv[]) {
+  setlocale(LC_ALL, "Portuguese");
+
+  int op;
+  float n1, n2, res;
+
+    printf("aperte 1 se quiser somar: \n");
+    printf("aperte 2 se quiser multiplicar: \n");
+    printf("aperte 3 se quiser subtrair: \n");
+    printf("aperte 4 se quiser dividir: \n");
+    scanf("%d", &op);
+
+    printf("escolha o primeiro numero:\n");
+    scanf("%f", &n1);
+    printf("escolha o segundo numero:\n");
+    scanf("%f", &n2);
+
+switch(op){
+    case 1:
+    res = n1 + n2;
+        printf("a soma e: %g\n", res);
+        break;
+    case 2:
+    res = n1 * n2;
+        printf("a multiplicao e: %g\n", res);
+        break;
+    case 3:
+    res = n1 - n2;
+        printf("a subtracao e: %g\n", res);
+        break;
+    case 4:
+        if(n2 == 0){
+            printf("divisao por zero");
+        }
+            else{
+             res = n1 / n2;
+                printf("a divisao e: %g\n", res);
+            }
+}
+
+    return 0;
+}
+
